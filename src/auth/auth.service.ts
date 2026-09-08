6dcea13896bca896a7494e025c88fffc188172b9
Ankit-subedi32 <ankit.subedi12322@gmail.com>
2026-09-08 09:35:46 +0545

    Database connection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/registerUser.dto';
+import  bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService : UserService){}
-    registerUser(registerUserDto: RegisterDto ){
+    async registerUser(registerUserDto: RegisterDto ){
         console.log("registerDto",registerUserDto);
+        const saltRounds = 10;
+        const hash = await bcrypt.hash(registerUserDto.password,saltRounds);
         //Logic for user register
         /**
          * 1. Check if email already exists
@@ -15,6 +18,6 @@ export class AuthService {
          * 4. generate jwt token 
          * 5. send token in response 
          */
-        return this.userService.createUser();
+        return this.userService.createUser({...registerUserDto,password:hash });
     }
 }
